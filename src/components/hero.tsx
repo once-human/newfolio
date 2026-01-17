a82097eb6c631c5635b6769c87eb5046c264a2d1
Onkar Yaglewad <onkar@portfolio.local>
2026-01-17 06:08:04 +0530

    style: max zoom 3.0x and subtle 15% grayscale filter

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -28,7 +28,7 @@ export function Hero() {
                             transition={{ delay: 0.5, type: "spring", stiffness: 100 }}
                             src="/assets/me.png"
                             alt="Profile"
-                            className="absolute z-0 w-[0.4em] h-[0.4em] object-cover rounded-full transition-all duration-500 ml-[0.04em] mt-[0.02em] hover:scale-[2.5]"
+                            className="absolute z-0 w-[0.4em] h-[0.4em] object-cover rounded-full grayscale-[0.15] hover:grayscale-0 transition-all duration-500 ml-[0.04em] mt-[0.02em] hover:scale-[3.0]"
                         />
                     </span>
                     NKA
